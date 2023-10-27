d7b832b2ae8a29edff3be2cb0628a8e06c5dc028
MehediShishir <shishir8866@gmail.com>
2023-10-27 21:12:41 +0600

    login added

--- Guest/urls.py
+++ Guest/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^login', views.login_view),
+    
+    
 ]
---
+++ Guest/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+# from .forms import SignUpForm
+from django.template import loader
+from user.models import *
+from datetime import *
+import re
+import os
+from django.contrib import messages
+
